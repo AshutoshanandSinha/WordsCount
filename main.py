00a4ff6489ab5efd359c09121e47bce493f2ea7a
ashu <ashutosh2431@gmail.com>
2020-04-23 14:00:17 -0700

    import changes in main

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Main wrapper function for the other functions.
 from word_count import topkwords
+import time
 
 def main():
+	start = time.time()
 	print("Starting up.")
-	data = topkwords('data/dataset-400MB.txt',10 )
+	data = topkwords('data/dataset-400MB.txt',50 )
 	if data.res:
-		print("Top {} words are :".format(10))
+		print("Top {} words are :".format(50))
 		print(data.res)
+	print(time.time()-start)
 if __name__ == "__main__":
 	main()
